4d0bfd1c2ff18b409c1d93d8096cb47dea1a91da
Srihari <sreehari7893640985@gmail.com>
2024-12-27 10:20:34 +1200

    Updated variables.java file

--- Java27122024/src/day1Java/Variables.java
+++ Java27122024/src/day1Java/Variables.java
@@ -6,7 +6,7 @@ public class Variables {
 	
 	public static void main(String[] args) {
 		 int a=10;
-		 int b=30;
+		 int b=30; // a,b are variables which holds some value
 		
 		System.out.println(a+b);
 	}
