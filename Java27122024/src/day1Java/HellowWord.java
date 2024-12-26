d81cf1f7c95731698e2f0888f2fd3d8dc825c389
Srihari <sreehari7893640985@gmail.com>
2024-12-27 10:13:20 +1200

    Day1 of Java Practice

---
+++ Java27122024/src/day1Java/HellowWord.java
@@ -0,0 +1,11 @@
+package day1Java;
+
+public class HellowWord {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");
+		System.out.println(3.+5);
+	}
+
+}
---
+++ Java27122024/src/day1Java/Variables.java
@@ -0,0 +1,13 @@
+package day1Java;
+
+public class Variables {
+	
+	
+	
+	public static void main(String[] args) {
+		 int a=10;
+		 int b=30;
+		
+		System.out.println(a+b);
+	}
+}
